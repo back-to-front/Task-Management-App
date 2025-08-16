39bdc3f1ea37ab95d4c6190f3e2e90922a3622d0
Cole <lock544@proton.me>
2025-08-15 20:13:33 -0500

    Updated landing page

--- Task-Management-App/src/pages/Landing.jsx
+++ Task-Management-App/src/pages/Landing.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Hero from "../components/Landing/Hero";
+import Features from "../components/Landing/Features";
+import CallToAction from "../components/Landing/CallToAction";
 
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <CallToAction />
+    </div>
+  );
 }
