08c29de2bfcc9c4c6df4a0ecbd9fba65260ec265
Cole <lock544@proton.me>
2025-08-15 19:44:30 -0500

    Updated the home page for the todo list

--- Task-Management-App/src/pages/Home.jsx
+++ Task-Management-App/src/pages/Home.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import TodoList from "../components/TodoList/TodoList";
+import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='home-container'>
+      <div className='home-content'>
+        <div className='home-header'>
+          <h1>Task Manager</h1>
+          <p>Organize, track, and complete your tasks efficiently</p>
+        </div>
+        <TodoList />
+      </div>
+    </div>
+  );
 }
