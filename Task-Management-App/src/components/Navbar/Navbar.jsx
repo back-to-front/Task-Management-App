7b61f2581923850a327486450c996a20feeb3ae7
Cole <lock544@proton.me>
2025-08-15 20:34:49 -0500

    Updated the navbar to support proper navigation between pages

--- Task-Management-App/src/components/Navbar/Navbar.jsx
+++ Task-Management-App/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { NavLink } from "react-router";
+import "./Navbar.css";
 
 function OffcanvasExample() {
   return (
@@ -12,7 +12,9 @@ function OffcanvasExample() {
       {["sm"].map((expand) => (
         <Navbar key={expand} expand={expand} className='bg-body-tertiary'>
           <Container fluid>
-            <Navbar.Brand href='#'>Task Management</Navbar.Brand>
+            <NavLink to='/' className='navLink navTitle'>
+              Task Management
+            </NavLink>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
@@ -26,21 +28,12 @@ function OffcanvasExample() {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className='justify-content-end flex-grow-1 pe-3'>
-                  <Nav.Link href='#action1'>Home</Nav.Link>
-                  <Nav.Link href='#action2'>Link</Nav.Link>
-                  <NavDropdown
-                    title='Dropdown'
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
-                    <NavDropdown.Item href='#action4'>
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href='#action5'>
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                  <NavLink to='/todo' className='navLink home'>
+                    Todos
+                  </NavLink>
+                  <NavLink to='/contact' className='navLink'>
+                    Contact
+                  </NavLink>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
